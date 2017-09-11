18659eb2e752195b77c5b5f45416aae34650b1d5
Stephane Plaisance <stephane.plaisance@vib.be>
2017-09-11 13:49:32 +0200

    edits

--- asm2cvg.sh
+++ asm2cvg.sh
@@ -21,10 +21,11 @@ usage='# Usage: asm2cvg.sh -r <reads> -a <assembly>
 # script version '${version}'
 # [optional: -w <window size|1000>]'
 
-while getopts "r:a:w:h" opt; do
+while getopts "r:a:t:x:w:h" opt; do
   case $opt in
     r) reads=${OPTARG} ;;
     a) assembly=${OPTARG} ;;
+    x) preset=$OPTARG ;;
     t) thread_opt=${OPTARG} ;;
     w) window_opt=${OPTARG} ;;
     h) echo "${usage}" >&2; exit 0 ;;
@@ -56,25 +57,18 @@ if [ ! -f "${assembly}" ]; then
 	exit 1
 fi
 
+# bwa mapping preset pacbio or nanopore
+if [ "${preset}" == "pacbio" || "${preset}" == "ont2d" ] ; then
+bwapreset="-x ${preset}"
+else
+bwapreset=""
+fi
+
 # defaults
 binwidth=${window_opt:-1000}
 threads=${thread_opt:-8}
+
 ref_index=${assembly%%.f*}
-
-# create bwa index if does not exists
-if [ -z "${ref_index}.bwt" ]; then
-bwa index -p ${ref_index} ${ref_index}.fasta
-fi
-
-ref_mappings="${reads%%.fastq*}_to_${assembly%%.f*}-alinments.bam"
-
-# if not done yet, map reads to index, sort results, and index bam
-if [ -z "${ref_mappings}" ]; then
-bwa mem -x pacbio -t ${threads} ${ref_index} ${reads} \
-  | samtools view -Sb - \
-  | samtools sort - \
-  > ${ref_mappings} && samtools index ${ref_mappings}
-fi
 
 # create genome size list from .fai indices
 samtools faidx ${assembly}
@@ -83,23 +77,63 @@ cut -f1,2 ${assembly}.fai > ${assembly}.sizes
 # create title lists for plots from fasta headers
 grep "^>" ${assembly} > ${assembly}.titles
 
+# create bwa index if does not exists
+if [ ! -f "${ref_index}.bwt" ]; then
+cmd="bwa index -p ${ref_index} ${ref_index}.fasta"
+
+echo "# ${cmd}"
+eval ${cmd}
+fi
+
+ref_mappings="${reads%%.fastq*}_to_${assembly%%.f*}-alignments.bam"
+
+# if not done yet, map reads to index, sort results, and index bam
+if [ ! -f "${ref_mappings}" ]; then
+cmd="bwa mem ${bwapreset} -t ${threads} ${ref_index} ${reads} \
+	| samtools view -Sb - \
+	| samtools sort - \
+	> ${ref_mappings} && samtools index ${ref_mappings}"
+	
+echo "# ${cmd}"
+eval ${cmd}
+fi
+
 # create windows
-bedtools makewindows -g ${assembly}.sizes -w ${binwidth} \
-	> ${assembly}.${binwidth}bp-bin.bed
+if [ ! -f "${assembly}.${binwidth}bp-bin.bed" ]; then
+cmd="bedtools makewindows -g ${assembly}.sizes -w ${binwidth} \
+	> ${assembly}.${binwidth}bp-bin.bed"
+
+echo "# ${cmd}"
+eval ${cmd}
+fi
 
 # bedtools genome wide coverage analysis
-bedtools coverage \
-  -b ${ref_mappings}\
+if [ ! -f "bedtools_coverage.${binwidth}bp_${assembly}-hist.txt" ]; then
+cmd="bedtools coverage -b ${ref_mappings}\
   -a ${assembly}.${binwidth}bp-bin.bed \
   -hist \
-  > bedtools_coverage.${binwidth}bp_${assembly}-hist.txt
-  
-# merge and generate stats in 1kb windows (ignore 'all' rows)
-bedtools groupby \
+  > bedtools_coverage.${binwidth}bp_${assembly}-hist.txt"
+
+echo "# ${cmd}"
+eval ${cmd}
+fi
+
+# merge and generate stats for each bin (ignore 'all' rows)
+if [ ! -f "bedtools_coverage.${binwidth}bp_${assembly}-stats.txt" ]; then
+cmd="bedtools groupby \
   -i <(grep -v "^all" bedtools_coverage.${binwidth}bp_${assembly}-hist.txt) \
-  -g 1,2,3 -c 5 -o min,median,mean,max \
-  > bedtools_coverage.${binwidth}bp_${assembly}-stats.txt
+  -g 1,2,3 \
+  -c 5 -o min,median,mean,max \
+  > bedtools_coverage.${binwidth}bp_${assembly}-stats.txt"
+
+echo "# ${cmd}"
+eval ${cmd}
+fi
 
 # run R plotting script
-btcvg2plots.R -b bedtools_coverage.${binwidth}bp_${assembly}-stats.txt \
-	-t ${assembly}.titles
+cmd="btcvg2plots.R -b bedtools_coverage.${binwidth}bp_${assembly}-stats.txt \
+	-t ${assembly}.titles"
+	
+echo "# ${cmd}"
+eval ${cmd}
+
