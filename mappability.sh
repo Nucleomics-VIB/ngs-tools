09cc54c1ce86efd1a96a16c3f35f7e3da8be6693
Stephane Plaisance <stephane.plaisance@vib.be>
2018-12-03 13:44:45 +0100

    edits

--- mappability.sh
+++ mappability.sh
@@ -14,30 +14,48 @@
 # a recent bash for capitalisation
 #
 # Stephane Plaisance (VIB-NC+BITS) 2017/05/19; v1.0
+# add --complement argument
 #
 # visit our Git: https://github.com/Nucleomics-VIB
 
-version="1.0, 2017_05_19"
+version="1.1, 2018_11_30"
 
 usage='# Usage: mappability.sh
-# -i <reference assembly fasta>)> 
+# -i <reference assembly fasta (not gzipped!)>)> 
 # -l <single read length for prediction (default to 100bps)>
 # -p <prefix for output data (default to input file prefix)>
-# -t <number of threads (default to 1)> 
+# -t <number of threads (default to 1)>
+# -b <index both strands <emulate|yes|no> (default=emulate)>
 # script version '${version}'
 # [-h for this help]'
 
-while getopts "i:l:p:t:h" opt; do
+while getopts "i:l:p:t:b:h" opt; do
   case $opt in
     i) opti=${OPTARG} ;;
     l) optl=${OPTARG} ;;
-    p) optp={OPTARGS} ;;
-    t) optt={OPTARGS} ;;
+    p) optp=${OPTARG} ;;
+    t) optt=${OPTARG} ;;
+    b) optb=${OPTARG} ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
   esac
 done
+
+#############################
+# check executables present
+declare -a arr=( "faSize" "gem-indexer" "gem-mappability" "gem-2-wig" "wigToBigWig" )
+for prog in "${arr[@]}"; do
+$( hash ${prog} 2>/dev/null ) || ( echo "# required ${prog} not found in PATH"; exit 1 )
+done
+
+# test provided option
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
 # declare variables
 infile=${opti}
@@ -58,27 +76,34 @@ idxpref="${pref}_index"
 kmer=${optl:-100}
 thr=${optt:-1}
 
+# test complement option
+comp=${optb:-"emulate"}
+ARRAY=( "emulate" "yes" "no" )
+containsElement "${comp}" "${ARRAY[@]}"
+if [ $? = 1 ]; then 
+  echo "## ERROR! invalid -b argument: ${comp}"
+  exit 1 
+fi
+
 # create new folder
 basedir=$(dirname ${infile})
 outdir="${basedir}/${pref}-mappability"
 mkdir -p ${outdir}
 
 # create reference size list
-$( hash faSize 2>/dev/null ) || ( echo "## ERROR! faSize not found in PATH"; exit 1 )
 echo "# creating reference size list"
 faSize ${infile} -detailed > ${outdir}/${pref}.sizes
 
 # create index
-$( hash gem-indexer 2>/dev/null ) || ( echo "## ERROR! gem-indexer not found in PATH"; exit 1 )
 echo "# creating GEM index ... be patient"
 gem-indexer -i ${infile} \
 	-o ${outdir}/${idxpref} \
+	--complement ${comp} \
 	-c 'dna' \
 	-T ${thr}
 
 # create mappability track
 echo "# creating GEM mappability data ... be patient"
-$( hash gem-mappability 2>/dev/null ) || ( echo "## ERROR!  gem-mappability not found in PATH"; exit 1 )
 gem-mappability -T ${thr} \
 	-I ${outdir}/${idxpref}.gem \
 	-l ${kmer} \
@@ -86,14 +111,12 @@ gem-mappability -T ${thr} \
 
 # convert to wig
 echo "# converting to WIG"
-$( hash gem-2-wig 2>/dev/null ) || ( echo "## ERROR!  gem-2-wig not found in PATH"; exit 1 )
 gem-2-wig -I ${outdir}/${idxpref}.gem \
 	-i ${outdir}/${pref}_${kmer}.mappability \
 	-o ${outdir}/${pref}_${kmer}
 
 # convert to BigWig
 echo "# converting to BigWIG"
-$( hash wigToBigWig 2>/dev/null ) || ( echo "## ERROR!  wigToBigWig not found in PATH"; exit 1 )
 wigToBigWig ${outdir}/${pref}_${kmer}.wig \
 	${outdir}/${pref}.sizes \
 	${outdir}/${pref}_${kmer}.bw
