558539644bd576de59a1cd9ba83ceb1db053fa91
Stephane Plaisance <stephane.plaisance@vib.be>
2020-06-05 17:30:33 +0200

    edits

---
+++ genmap_mappability.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+# script name: genmap_mappability.sh
+# create genmap indices from a fasta reference
+# create mappability track for a given read length
+#
+## Requirements:
+# genmap from https://github.com/cpockrandt/genmap
+# bioawk for reference cleaning (https://github.com/lh3/bioawk)
+# a recent bash for capitalisation
+#
+# Stephane Plaisance (VIB-NC+BITS) 2020/06/05; v1.0
+#
+# visit our Git: https://github.com/Nucleomics-VIB
+
+version="1.01, 2020_06_05"
+
+usage='# Usage: genmap_mappability.sh
+# -i <reference assembly fasta>
+# -l <single read length for prediction (default to 100bps)>
+# -p <prefix for output data (default to "genmap_mappability")>
+# -e <max errors (0-4, default to 4)>
+# -t <add detailed AND LARGE text output (off by default)>
+# -f <count only for contigs starting with prefix (default to none, eg "chr")>
+# script version '${version}'
+# [-h for this help]'
+
+while getopts "i:l:p:e:f:th" opt; do
+  case $opt in
+    i) opti=${OPTARG} ;;
+    l) optl=${OPTARG} ;;
+    p) optp=${OPTARG} ;;
+    e) opte=${OPTARG} ;;
+    t) optt=" -t ";;
+    f) optf=${OPTARG} ;;
+    h) echo "${usage}" >&2; exit 0 ;;
+    \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
+    *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
+  esac
+done
+
+# test if minimal arguments were provided
+infile=${opti}
+if [ -z "${infile}" ]
+then
+  echo "# no fasta provided!"
+  echo "${usage}"
+  exit 1
+fi
+
+# check if exists or die
+[ -f ${infile} ] || ( echo "## ERROR! Fasta reference file not found" ; exit 1 )
+
+ref=$(basename ${infile%.fa*})
+kmer=${optl:-100}
+err=${opte:-4}
+filter=${optf:-""}
+
+####################
+# custom functions #
+####################
+
+function countmappability() {
+gawk -v min=$1 'BEGIN{FS="\t"; OFS=","; tot=0} \
+  {if ($4>=min/10000) tot=tot+$3-$2} \
+  END{printf "%d,%d\n", min/100, tot}' $2
+  }
+export -f countmappability
+
+ranok () {
+if [ $? -ne 0 ]
+then
+echo "!! something went wrong, quitting."
+exit 1
+fi
+}
+export -f ranok
+
+#####################
+# create outfolders #
+#####################
+
+outdir=${optp:-"$(pwd)/genmap_mappability"}
+mkdir -p ${outdir}/tmp
+export TMPDIR=${outdir}/tmp
+
+#######################
+# create index (once) #
+#######################
+
+if [ ! -f ${outdir}/genmap_${ref}_idx_created ]; then
+
+echo "# creating genmap index for ${ref}"
+
+genmap index \
+-F ${infile} \
+-I ${outdir}/genmap_${ref}_idx
+
+touch ${outdir}/genmap_${ref}_idx_created
+else
+echo "# genmap index already present; delete genmap_${ref}_idx and re-run to reindex"
+fi
+
+#######################
+# compute mappability #
+#######################
+
+if [ ! -f ${outdir}/genmap_${ref}_${kmer}_${err}_computed ]; then
+
+echo "# computing mappability on ${ref} for kmer:${kmer} and max ${err} errors"
+genmap map \
+-I ${outdir}/genmap_${ref}_idx \
+-O ${outdir}/genmap_${ref}_${kmer}_${err} \
+-K ${kmer} \
+-E ${err} \
+-w \
+-bg ${optt}
+
+ranok
+
+touch ${outdir}/genmap_${ref}_${kmer}_${err}_computed
+else
+echo "# mappability already computed for kmer=${kmer} and ${err} error(s), please delete ${outdir}/genmap_${kmer}_${err}_computed to recompute"
+fi
+
+#################
+# filter counts #
+#################
+
+echo "# plotting mappability on ${ref} for kmer:${kmer} and max ${err} errors"
+
+bedg=${outdir}/genmap_${ref}_${kmer}_${err}.bedgraph
+
+if [ -n ${optf} ]; then
+echo "# counting only contigs starting by: ${optf}"
+filter="_${optf}"
+data=${outdir}/genmap_${ref}_${kmer}_${err}${filter}.bedgraph
+# filter bedgraph data
+gawk 'BEGIN{FS="\t"; OFS="\t"}{if ($1~/'${optf}'/) print $0}' \
+${bedg} \
+> ${data}
+else
+echo "counting all contigs in reference"
+filter=""
+data=${bedg}
+fi
+
+ranok
+
+####################
+# summaryze counts #
+####################
+
+echo "# summarizing base counts from ${data}"
+
+counts=${outdir}/genmap_${ref}_${kmer}_${err}${filter}_counts.txt
+echo "min-mappability_pc,bases" > ${counts}
+(for (( i=1; $i<=10; i++ )); do echo 10000/$i |bc; done; echo 0) \
+|parallel 'countmappability {} '${data} \
+|sort --field-separator=',' -k 1n,1 \
+>> ${counts}
+
+ranok
+
+####################
+# plot from counts #
+####################
+
+echo "# plotting from summarised counts"
+
+plot=${outdir}/mappability_${ref}_${kmer}_${err}${filter}_plot.png
+
+gnuplot <<-EOFMarker
+set datafile separator ","
+set key autotitle columnhead
+set style line 1 \
+  linecolor rgb '#0060ad' \
+  linetype 1 linewidth 2 \
+  pointtype 7 pointsize 1.5
+set yrange [*:100]
+set xlabel "mappability % (kmer: '${kmer}')"
+set ylabel "% of genome width above map%"
+max(a,b)=a>b?a:b
+a=-99999999.
+set terminal pngcairo
+set output '${plot}'
+plot '${counts}' using 1:(a=max(a,column(2)),0/0) notitle, \
+  '' using 1:(100*column(2)/a) notitle with linespoints linestyle 1
+EOFMarker
+
+echo "# results have been written to: mappability_${ref}_${kmer}_${err}_plot.png"
--- mappability.sh
+++ mappability.sh
@@ -58,15 +58,24 @@ containsElement () {
   return 1
 }
 
+# command failed
+ranok () {
+if [ $? -ne 0 ]
+then
+echo "!! something went wrong, quitting."
+exit 1
+fi
+}
+
 # declare variables
 infile=${opti}
 
 # test if minimal arguments were provided
 if [ -z "${infile}" ]
 then
-   echo "# no fasta provided!"
-   echo "${usage}"
-   exit 1
+  echo "# no fasta provided!"
+  echo "${usage}"
+  exit 1
 fi
 
 # check if exists or die
@@ -93,81 +102,140 @@ outdir="${basedir}/${pref}-mappability"
 mkdir -p ${outdir}
 
 # clean reference fasta file (names with space or comments)
+if [ ! -f ${outdir}/${refname}.sizes ]; then
+
 echo "# creating a clean reference and size list"
 bioawk -c fastx '{printf(">%s\n%s\n",$name,$seq)}' ${infile} \
-	> ${outdir}/${refname}.fa
+  > ${outdir}/${refname}.fa 
+
+ranok
 
 bioawk -c fastx '{ printf("%s\t%s\n",$name,length($seq)) }' ${infile} \
-	> ${outdir}/${refname}.sizes
+  > ${outdir}/${refname}.sizes
+
+ranok
+
+fi
 
 # create index
+if [ ! -f ${outdir}/${idxpref}.gem ]; then
+
 echo "# creating GEM index ... be patient"
 gem-indexer -i ${outdir}/${refname}.fa \
-	-o ${outdir}/${idxpref} \
-	--complement ${comp} \
-	-c 'dna' \
-	-T ${thr}
+  -o ${outdir}/${idxpref} \
+  --complement ${comp} \
+  -c 'dna' \
+  -T ${thr}
+
+ranok
+
+fi
 
 # create mappability track
+if [ ! -f ${outdir}/${pref}_${kmer}.mappability ]; then
+
 echo "# creating GEM mappability data ... be patient"
 gem-mappability -T ${thr} \
-	-I ${outdir}/${idxpref}.gem \
-	-l ${kmer} \
-	-o ${outdir}/${pref}_${kmer}
+  -I ${outdir}/${idxpref}.gem \
+  -l ${kmer} \
+  -o ${outdir}/${pref}_${kmer}
+
+ranok
 
 # convert to wig
 echo "# converting to WIG"
 gem-2-wig -I ${outdir}/${idxpref}.gem \
-	-i ${outdir}/${pref}_${kmer}.mappability \
-	-o ${outdir}/${pref}_${kmer}
+  -i ${outdir}/${pref}_${kmer}.mappability \
+  -o ${outdir}/${pref}_${kmer}
+
+ranok
+
+fi
 
 # convert to BigWig
+if [ ! -f ${outdir}/${pref}_${kmer}.bw ]; then
+
 echo "# converting to BigWIG"
 wigToBigWig ${outdir}/${pref}_${kmer}.wig \
-	${outdir}/${refname}.sizes \
-	${outdir}/${pref}_${kmer}.bw
+  ${outdir}/${refname}.sizes \
+  ${outdir}/${pref}_${kmer}.bw
+
+ranok
+
+fi
 
 # convert to bedGraph & BED
+if [ ! -f ${outdir}/${pref}_${kmer}.bedGraph ]; then
+
 echo "# converting to BED"
 bigWigToBedGraph ${outdir}/${pref}_${kmer}.bw \
-	${outdir}/${pref}_${kmer}.bedGraph
-	
+  ${outdir}/${pref}_${kmer}.bedGraph
+
+ranok
+
+fi
+
 # keep only regions with mappability > min
+if [ ! -f ${outdir}/${pref}_${kmer}_gt_${min}.bed ]; then
+
 min=${optm:-0.2}
 gawk -v min=${min} 'BEGIN{FS="\t"; OFS="\t"}
-	{if ($4>=min) print $1 $2 $3 "mappability_gt_"min $4}' \
-	${outdir}/${pref}_${kmer}.bedGraph \
-	> ${outdir}/${pref}_${kmer}_gt_${min}.bed
+  {if ($4>=min) print $1,$2,$3,"mappability_gt_"min,$4}' \
+  ${outdir}/${pref}_${kmer}.bedGraph \
+  > ${outdir}/${pref}_${kmer}_gt_${min}.bed
 
-# produce counts for plotting
+ranok
+
+fi
+
 # count bases above mappability threshold in range 0:100% by 5% steps
-lims=( $(seq 0 5 100) )
-echo "min-mappability_pc,bases" > ${outdir}/counts.txt 
-# loop and add counts to file
-for thr in ${lims[*]}; do
-gawk -v min=${thr} 'BEGIN{FS="\t"; OFS="\t"; tot=0} \
-	{if ($4>=min/100) tot=tot+$3-$2}\
-	END{print min","tot}' \
-	${outdir}/${pref}_${kmer}.bedGraph
-done >> ${outdir}/counts.txt
+if [ ! -f ${outdir}/counts_${kmer}.txt ]; then
+
+# echo "min-mappability_pc,bases" > ${outdir}/counts_${kmer}.txt 
+# loop and add counts to file (takes time!)
+# lims=( $(seq 0 5 100) )
+# for thr in ${lims[*]}; do
+# gawk -v min=${thr} 'BEGIN{FS="\t"; OFS=","; tot=0} \
+#   {if ($4>=min/100) tot=tot+$3-$2} \
+#   END{print min,tot}' \
+#   ${outdir}/${pref}_${kmer}.bedGraph
+# done >> ${outdir}/counts.txt
+
+# same but using gnu-parallel and || threads
+function countmappability() {
+gawk -v min=$1 'BEGIN{FS="\t"; OFS=","; tot=0} \
+  {if ($4>=min/10000) tot=tot+$3-$2} \
+  END{printf "%d,%d\n", min/100, tot}' $2
+  }
+
+export -f countmappability
+
+echo "min-mappability_pc,bases" > ${outdir}/counts_${kmer}.txt 
+
+(for (( i=1; $i<=10; i++ )); do echo 10000/$i |bc; done; echo 0) |parallel 'countmappability {} '${outdir}/${pref}_${kmer}.bedGraph \
+| sort --field-separator=',' -k 1n,1 >> ${outdir}/counts_${kmer}.txt
+
+ranok
 
 # plot it to png
 gnuplot <<-EOFMarker
 set datafile separator ","
 set key autotitle columnhead
 set style line 1 \
-    linecolor rgb '#0060ad' \
-    linetype 1 linewidth 2 \
-    pointtype 7 pointsize 1.5
+  linecolor rgb '#0060ad' \
+  linetype 1 linewidth 2 \
+  pointtype 7 pointsize 1.5
 set yrange [*:100]
 set xlabel "mappability %"
-set ylabel "% above threshold"
+set ylabel "% of genome width above map%"
 max(a,b)=a>b?a:b
 a=-99999999.
 set terminal pngcairo
-set output '${outdir}/mappability_plot.png'
-plot '${outdir}/counts.txt' using 1:(a=max(a,column(2)),0/0) notitle, \
-    '' using 1:(100*column(2)/a) notitle with linespoints linestyle 1
+set output '${outdir}/mappability_${kmer}_plot.png'
+plot '${outdir}/counts_${kmer}.txt' using 1:(a=max(a,column(2)),0/0) notitle, \
+  '' using 1:(100*column(2)/a) notitle with linespoints linestyle 1
 EOFMarker
+
+fi
 
 echo "# all done."
