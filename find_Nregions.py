341267575b938fc2d16d24da275e8ab62fa74f3c
Stephane Plaisance <stephane.plaisance@vib.be>
2020-06-11 21:40:07 +0200

    Create find_Nregions.py

---
+++ find_Nregions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# find_Nregions.py
+# save as bed track
+
+# Import necessary packages
+import argparse
+import re
+from Bio import SeqIO
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta")
+args = parser.parse_args()
+
+# Open FASTA, search for masked regions, print in BED5 format
+with open(args.fasta) as handle:
+    for record in SeqIO.parse(handle, "fasta"):
+        for match in re.finditer('N+', str(record.seq)):
+            print(record.id, match.start(), match.end(), "gap", match.end()-match.start(), "+", sep='\t')
