47016a4bacf8b99eeebd707a4706f1f50c4230fd
Stephane Plaisance <stephane.plaisance@vib.be>
2025-03-26 15:09:21 +0100

    Update run_hostile_hs_decontamination.sh

--- run_hostile_hs_decontamination.sh
+++ run_hostile_hs_decontamination.sh
@@ -4,7 +4,7 @@
 # Usage: ./run_hostile_hs_decontamination.sh -1 <read1.fastq.gz> -2 <read2.fastq.gz> -o <output_prefix> [OPTIONS]
 # Description: Decontaminates paired-end FASTQ files using hostile by removing reads aligning to the human genome.
 # Hostile Repository: https://github.com/czbiohub/hostile
-# Author: Sp@NC (+AI)
+# Author: SP@NC (+AI)
 # Date: 2025-03-17
 # Version: 1.0
 #
@@ -37,18 +37,18 @@ usage() {
   echo "Required Arguments:"
   echo "  -1, --read1 <file>      Path to read 1 FASTQ file (gzipped)."
   echo "  -2, --read2 <file>      Path to read 2 FASTQ file (gzipped)."
-  echo "  -o, --output <prefix>   Output file prefix. Output files will be placed in $OUTPUT_DIR and named:"
-  echo "                          $OUTPUT_DIR/<prefix>.R1.decontaminated.fastq.gz"
-  echo "                          $OUTPUT_DIR/<prefix>.R2.decontaminated.fastq.gz"
+  echo "  -o, --output <prefix>   Output file prefix. Output files will be placed in $OUTPUT and named:"
+  echo "                          $OUTPUT/<prefix>.R1.clean.fastq.gz"
+  echo "                          $OUTPUT/<prefix>.R2.clean.fastq.gz"
   echo ""
   echo "Optional Arguments:"
+  echo "  -a, --aligner {bowtie2,minimap2,auto} Alignment algorithm (auto by default)"
   echo "  -i, --index <path>      Path to the hostile index.  Defaults to '$HOSTILE_INDEX'"
   echo "  -t, --threads <int>     Number of threads to use. Defaults to '$THREADS'"
-  echo "  -a, --aligner {bowtie2,minimap2,auto} Alignment algorithm (auto by default)"
   echo "  -h, --help              Show this help message and exit."
   echo ""
   echo "Example:"
-  echo "  $0 -1 read1.fastq.gz -2 read2.fastq.gz -o output -t 8 -i /path/to/index -a bowtie2"
+  echo "  $0 -1 read1.fastq.gz -2 read2.fastq.gz -o output -t 8 -a bowtie2 -i /path/to/index"
   exit 1
 }
 
@@ -100,10 +100,10 @@ mkdir -p "$OUTPUT"
 echo "Running hostile clean with the following parameters:"
 echo "  Read 1: $READ1"
 echo "  Read 2: $READ2"
-echo "  Output Directory: $OUTPUT"
+echo "  Aligner: $ALIGNER"
 echo "  Index: $INDEX"
 echo "  Threads: $THREADS"
-echo "  Aligner: $ALIGNER"
+echo "  Output Directory: $OUTPUT"
 
 # Construct the hostile clean command
 HOSTILE_CMD="hostile clean --fastq1 \"$READ1\" --fastq2 \"$READ2\" --index \"$INDEX\" -t \"$THREADS\" -o \"$OUTPUT\" --aligner \"$ALIGNER\""
